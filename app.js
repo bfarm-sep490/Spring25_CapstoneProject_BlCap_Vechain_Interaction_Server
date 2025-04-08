974090446a3a236c8da9d4174a03e08511062840
thangbinhbeo <binhbeopro1122@gmail.com>
2025-04-08 15:30:17 +0700

    chore: 🥀 fix app.js

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const PORT = 3000;
 
 const app = express();
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin:  '*',  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.get("/", (req, res) => {
